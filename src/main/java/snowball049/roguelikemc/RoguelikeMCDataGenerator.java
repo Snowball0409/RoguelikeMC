95a43a14b9cbbb0b32101ab9294fe21eb3140d3d
Snowball0409 <kai890409@gmail.com>
2025-03-27 21:49:21 +0800

    Implement datagen method

--- src/main/java/snowball049/roguelikemc/RoguelikeMCDataGenerator.java
+++ src/main/java/snowball049/roguelikemc/RoguelikeMCDataGenerator.java
@@ -2,10 +2,145 @@ package snowball049.roguelikemc;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.data.server.advancement.AdvancementProvider;
+import snowball049.roguelikemc.datagen.RoguelikeMCUpgradeDataProvider;
 
 public class RoguelikeMCDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider((FabricDataOutput output) -> {
+			RoguelikeMCUpgradeDataProvider upgradeProvider = new RoguelikeMCUpgradeDataProvider(output);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("health_1", "+1 Health")
+							.description("Add 1 Health")
+							.tier("common")
+							.isPermanent(false)
+							.isUnique(false)
+							.icon("minecraft:textures/mob_effect/regeneration.png")
+							.addEffectAction("attribute", "minecraft:generic.max_health", "1", "add_value")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("attack_1", "+1 Attack")
+							.description("Add 1 Attack")
+							.tier("common")
+							.isPermanent(false)
+							.isUnique(false)
+							.icon("minecraft:textures/mob_effect/strength.png")
+							.addEffectAction("attribute", "minecraft:generic.attack_damage", "1", "add_value")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("speed_10", "+10% Speed")
+							.description("Add 10% Speed")
+							.tier("common")
+							.isPermanent(true)
+							.isUnique(false)
+							.icon("minecraft:textures/mob_effect/speed.png")
+							.addEffectAction("attribute", "minecraft:generic.movement_speed", "0.1", "add_multiplied_total")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("dragon_skin", "Dragon Skin")
+							.description("Get resistant I but minus 1 Heart")
+							.tier("rare")
+							.isPermanent(false)
+							.isUnique(true)
+							.icon("minecraft:textures/item/dragon_breath.png")
+							.addEffectAction("attribute", "minecraft:generic.max_health", "-2", "add_value")
+							.addEffectAction("effect", "minecraft:resistance", "-1", "0")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("phoenix_feather", "Phoenix Feather")
+							.description("Grants regeneration effect and feather falling effect")
+							.tier("epic")
+							.isPermanent(false)
+							.isUnique(false)
+							.icon("minecraft:textures/item/feather.png")
+							.addEffectAction("effect", "minecraft:regeneration", "-1", "0")
+							.addEffectAction("effect", "minecraft:slow_falling", "-1", "0")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("titan_strength", "Titan Strength")
+							.description("Increases attack damage by 5")
+							.tier("legendary")
+							.isPermanent(true)
+							.isUnique(false)
+							.icon("minecraft:textures/mob_effect/strength.png")
+							.addEffectAction("attribute", "minecraft:generic.attack_damage", "5", "add_value")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("swift_boots", "Swift Boots")
+							.description("Increases movement speed by 10%")
+							.tier("common")
+							.isPermanent(false)
+							.isUnique(true)
+							.icon("minecraft:textures/mob_effect/speed.png")
+							.addEffectAction("attribute", "minecraft:generic.movement_speed", "0.1", "add_multiplied_base")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("iron_hide", "Iron Hide")
+							.description("Increases armor by 3")
+							.tier("rare")
+							.isPermanent(false)
+							.isUnique(false)
+							.icon("minecraft:textures/item/iron_ingot.png")
+							.addEffectAction("attribute", "minecraft:generic.armor", "3", "add_value")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("shadow_cloak", "Shadow Cloak")
+							.description("Grants invisibility effect for 5 seconds")
+							.tier("epic")
+							.isPermanent(true)
+							.isUnique(true)
+							.icon("minecraft:textures/mob_effect/invisibility.png")
+							.addEffectAction("effect", "minecraft:invisibility", "-1", "0")
+			);
+
+			// Note: Commented out event actions are left out
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("arcane_barrier", "Arcane Barrier")
+							.description("Grants absorption for 30 seconds")
+							.tier("epic")
+							.isPermanent(false)
+							.isUnique(true)
+							.icon("minecraft:textures/item/ender_eye.png")
+							.addEffectAction("effect", "minecraft:absorption", "-1", "0")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("berserker_rage", "Berserker Rage")
+							.description("Increases attack speed but reduces defense")
+							.tier("legendary")
+							.isPermanent(true)
+							.isUnique(true)
+							.icon("minecraft:textures/item/golden_sword.png")
+							.addEffectAction("attribute", "minecraft:generic.attack_speed", "0.5", "add_value")
+							.addEffectAction("attribute", "minecraft:generic.armor", "-2", "add_value")
+			);
+
+			upgradeProvider.addUpgrade(
+					new RoguelikeMCUpgradeDataProvider.UpgradeData("arcane_focus", "Arcane Focus")
+							.description("Increases mana regeneration by 15%")
+							.tier("common")
+							.isPermanent(false)
+							.isUnique(true)
+							.icon("minecraft:textures/item/ender_pearl.png")
+							.addEffectAction("attribute", "minecraft:generic.max_health", "1", "add_value")
+							.addEffectAction("effect", "minecraft:haste", "-1", "0")
+			);
+
+			return upgradeProvider;
+		});
 	}
 }
---
+++ src/main/java/snowball049/roguelikemc/datagen/RoguelikeMCUpgradeDataProvider.java
@@ -0,0 +1,141 @@
+package snowball049.roguelikemc.datagen;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.DataWriter;
+import snowball049.roguelikemc.RoguelikeMC;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class RoguelikeMCUpgradeDataProvider implements DataProvider {
+    private final FabricDataOutput output;
+    private final List<UpgradeData> upgrades = new ArrayList<>();
+
+    public RoguelikeMCUpgradeDataProvider(FabricDataOutput output) {
+        this.output = output;
+    }
+
+    // Nested class to represent upgrade data
+    public static class UpgradeData {
+        private final String id;
+        private final String name;
+        private String description = "";
+        private String tier = "common";
+        private boolean isPermanent = false;
+        private boolean isUnique = false;
+        private String icon = "";
+        private final List<ActionData> actions = new ArrayList<>();
+
+        public UpgradeData(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public UpgradeData description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public UpgradeData tier(String tier) {
+            this.tier = tier;
+            return this;
+        }
+
+        public UpgradeData isPermanent(boolean isPermanent) {
+            this.isPermanent = isPermanent;
+            return this;
+        }
+
+        public UpgradeData isUnique(boolean isUnique) {
+            this.isUnique = isUnique;
+            return this;
+        }
+
+        public UpgradeData icon(String icon) {
+            this.icon = icon;
+            return this;
+        }
+
+        public UpgradeData addEffectAction(String type, String effect, String duration, String amplifier) {
+            actions.add(new ActionData(type, effect, duration, amplifier));
+            return this;
+        }
+
+        // Inner class for action data
+        public static class ActionData {
+            private final String type;
+            private final List<String> value;
+
+            public ActionData(String type, String effect, String duration, String amplifier) {
+                this.type = type;
+                this.value = List.of(effect, duration, amplifier);
+            }
+        }
+
+        // Convert to JsonObject for serialization
+        public JsonObject toJson() {
+            JsonObject json = new JsonObject();
+            json.addProperty("id", id);
+            json.addProperty("name", name);
+            json.addProperty("description", description);
+            json.addProperty("tier", tier);
+            json.addProperty("is_permanent", isPermanent);
+            json.addProperty("is_unique", isUnique);
+            json.addProperty("icon", icon);
+
+            JsonArray actionArray = new JsonArray();
+            for (ActionData action : actions) {
+                JsonObject actionObj = new JsonObject();
+                actionObj.addProperty("type", action.type);
+
+                JsonArray valueArray = new JsonArray();
+                action.value.forEach(valueArray::add);
+
+                actionObj.add("value", valueArray);
+                actionArray.add(actionObj);
+            }
+            json.add("action", actionArray);
+
+            return json;
+        }
+    }
+
+    // Method to add upgrades
+    public void addUpgrade(UpgradeData upgrade) {
+        upgrades.add(upgrade);
+    }
+
+    @Override
+    public CompletableFuture<?> run(DataWriter writer) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<CompletableFuture<?>> futures = new ArrayList<>();
+
+        for (UpgradeData upgrade : upgrades) {
+            JsonObject upgradeJson = upgrade.toJson();
+
+            Path path = output.getPath()
+                    .resolve(FabricLoader.getInstance().getGameDir())
+                    .resolve("data")
+                    .resolve(RoguelikeMC.MOD_ID)
+                    .resolve("upgrades")
+                    .resolve(upgrade.id + ".json");
+
+            futures.add(DataProvider.writeToPath(writer, upgradeJson, path));
+        }
+
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+    }
+
+    @Override
+    public String getName() {
+        return "RoguelikeMC Upgrade Data Provider";
+    }
+}
