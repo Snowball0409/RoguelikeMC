b0bef619a7266a8da085b40431d2be4567a12eed
Snowball0409 <kai890409@gmail.com>
2025-04-09 00:36:26 +0800

    Remove unnecessary file

--- backup/main/roguelikemc/compat/RoguelikeMCCompat.java
+++
@@ -1,9 +0,0 @@
-package snowball049.roguelikemc.compat;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class RoguelikeMCCompat {
-    public static boolean isModLoaded(String modId) {
-        return FabricLoader.getInstance().isModLoaded(modId);
-    }
-}
